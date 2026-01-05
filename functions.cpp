ff6e9ae3ce19c6d601a20f1412d76b3413d60208
Christian Strasser <chstrasser3@gmail.com>
2026-01-05 16:49:42 +0100

    fixed some indexing issues

--- functions.cpp
+++ functions.cpp
@@ -17,12 +17,13 @@ int solveJacobi2D_C(const double L
                    ,double* const aux
                    ,std::ofstream& LOG_FILE) {
 
-   for (int i = 0; i < NX; ++i) {          
+   for (int i = 1; i < NX; ++i) {          
 
-         sol[ij2l(i,NX-1,NX)] = sin(M_PI*i*L/NX);// north boundary
-         sol[ij2l(i,0,NX)] = 0.0;                // south boundary
-         sol[ij2l(0,i,NX)] = 0.0;                // west boundary
+         
+         sol[ij2l(i,1,NX)] = 0.0;                // south boundary
+         sol[ij2l(1,i,NX)] = 0.0;                // west boundary
          sol[ij2l(NX-1,i,NX)] = 0.0;             // east boundary
+         sol[ij2l(i,NX-1,NX)] = sin(M_PI*i*L/NX);// north boundary
 
       }
 
@@ -32,24 +33,16 @@ int solveJacobi2D_C(const double L
    iters = 0;
    while (res>TOL) {
          
-      const double DX  = L/static_cast<double>(NX);
-      const double DY  = L/static_cast<double>(NY);
-      const double DXSQ  = pow(DX,2);
-      const double DYSQ  = pow(DY,2);
-      const double denom = 2.0 / DXSQ + 2.0 / DYSQ;
       
-      for (int j=2; j<NY-2; ++j) {
-         for (int i=2; i<NX-2; ++i) {
-
+      for (int j=2; j<NY-1; ++j) {
+         for (int i=2; i<NX-1; ++i) {
          const int l11 = ij2l(i,j,NX);
          const int l01 = ij2l(i,j-1,NX);
          const int l10 = ij2l(i-1,j,NX);
          const int l21 = ij2l(i+1,j,NX);
          const int l12 = ij2l(i,j+1,NX);
 
-         aux[l11] =((sol[l21] + sol[l10]) / DXSQ
-                  + (sol[l12] + sol[l01]) / DYSQ
-                  ) / denom;
+         aux[l11] =(sol[l21] + sol[l10] + sol[l12] + sol[l01]) * 0.25;
          }
       }
 
