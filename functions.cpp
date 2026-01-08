4cf732186406d11ebe206f4b64260757488d7ebd
Christian Strasser <e12009631@student.tuwien.ac.at>
2026-01-08 11:43:48 +0100

    add the other solution, fixed my solution

--- functions.cpp
+++ functions.cpp
@@ -25,7 +25,7 @@ int solveJacobi2D_C(const double L
          sol[ij2l(i,1,NX)] = 0.0;                // south boundary
          sol[ij2l(1,i,NX)] = 0.0;                // west boundary
          sol[ij2l(NX-1,i,NX)] = 0.0;             // east boundary
-         sol[ij2l(i,NX-1,NX)] = sin(M_PI*i*L/NX);// north boundary
+         sol[ij2l(i,NX-1,NX)] = sin(M_PI*i*L/(NX-1));// north boundary
 
       }
 
@@ -35,8 +35,7 @@ int solveJacobi2D_C(const double L
    iters = 0;
 
    while (res>TOL) {
-         
-    // Calculate the next step and store it in aux[]
+      // Calculate the next step and store it in aux[]
       for (int j=2; j<NY-1; ++j) {
          for (int i=2; i<NX-1; ++i) {
          const int l11 = ij2l(i,j,NX);
@@ -48,6 +47,15 @@ int solveJacobi2D_C(const double L
          aux[l11] =(sol[l21] + sol[l10] + sol[l12] + sol[l01]) * 0.25;
          }
       }
+
+      //update sol with the next solution 
+      for (int j=2; j<NY-1; ++j) {
+         for (int i=2; i<NX-1; ++i) {
+           const int l = ij2l(i,j,NX);
+         sol[l] = aux[l];
+         }
+      }
+
 
 
       res = 0.0;            
@@ -67,15 +75,18 @@ int solveJacobi2D_C(const double L
          const int l21 = ij2l(i+1,j,NX);
          const int l12 = ij2l(i,j+1,NX);
 
-         res += abs(aux[l21] + aux[l10] + aux[l12] + aux[l01] - 4*aux[l11]);
+         res += abs(sol[l21] + sol[l10] + sol[l12] + sol[l01] - 4*sol[l11]);
          }
       }
       res /= sqrt(N);
 
 
       //update sol with the next solution 
-      for (int l = 0; l<N;++l) {
+      for (int j=2; j<NY-1; ++j) {
+         for (int i=2; i<NX-1; ++i) {
+           const int l = ij2l(i,j,NX);
          sol[l] = aux[l];
+         }
       }
 
       ++iters;
@@ -86,6 +97,29 @@ int solveJacobi2D_C(const double L
       LOG_FILE << "Successfull convergence" << endl;
       LOG_FILE << " - residual achieved " << res << endl;
       LOG_FILE << " - iterations " << iters << endl;
+
+
+      ofstream SOL("Sol.log");
+
+      for (int j=1; j<NY; ++j) {
+          for (int i=1; i<NX; ++i) {
+            if (i != NX-1){
+              SOL << sol[ij2l(i,j,NX)] << ",";
+            }
+            else{
+              SOL << sol[ij2l(i,j,NX)]; 
+            }
+           
+           }
+         if (j == NY-1){
+
+         }
+         else{
+           SOL << endl;
+         }
+       }
+
+
       return EXIT_SUCCESS;
    } else { 
       LOG_FILE << "Failure of convergence procedure" << endl;
